327b10abc032a6ffb00aa058a1b8b994d856ef14
Ugonz86 <ugonzalez86@gmail.com>
2021-02-01 11:48:11 -0800

    Add form handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import '../index.css';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    // onInputChange(event) {
+    //     console.log(event.target.value)
+    // }
+
+    // onInputClick() {
+    //     console.log('input was clicked')
+    // }
+    
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term) 
+    }
+
     render() {
         return (
             <div className="ui segment" >
-                <form className="ui form" >
+                <form onSubmit={this.onFormSubmit} className="ui form" >
                     <div className="field" >
-                    <label>Image Search</label>
-                        <input type="text"></input>
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) => this.setState({ term: e.target.value })} >
+                        </input>
                     </div>
                 </form>
             </div>
