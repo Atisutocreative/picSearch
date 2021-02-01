485c7b017bb5ee9334d403b10924239f948091c5
Ugonz86 <ugonzalez86@gmail.com>
2021-02-01 12:56:55 -0800

    Update functionality to be able to handle requests with async await

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,26 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-
-  onSearchSubmit(term) {
-    axios
+  state = { images: [] };
+  //Callback function
+  onSearchSubmit = async (term) => { 
+    //By putting the async keyword allow us to use the await syntax inside of this function.
+    //async means that a function always returns a promise.
+    const response = await axios //The keyword await makes JavaScript wait until that promise settles and returns its result.
       .get('https://api.unsplash.com/search/photos', {
         params: { query: term },
         headers: {
           Authorization: 'Client-ID UmBUTENy_-knJN2pyTwE8PQF-zJNiK2oGFdz9rLfNPQ',
         }
-      }).then((response) => {
-        console.log(response.data.results);
       });
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
