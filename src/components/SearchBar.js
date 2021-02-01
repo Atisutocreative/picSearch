aefc465fc27bcba8bf3ad2c8eb87a1a0138bd10f
Ugonz86 <ugonzalez86@gmail.com>
2021-02-01 11:49:48 -0800

    Turn App into Class component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,14 +2,6 @@ import React from 'react';
 
 class SearchBar extends React.Component {
     state = { term: '' };
-
-    // onInputChange(event) {
-    //     console.log(event.target.value)
-    // }
-
-    // onInputClick() {
-    //     console.log('input was clicked')
-    // }
     
     onFormSubmit = (event) => {
         event.preventDefault();
