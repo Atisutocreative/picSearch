1a403adacea329e2669d80c7ad6b7ad8d36d739e
Ugonz86 <ugonzalez86@gmail.com>
2021-02-01 12:34:17 -0800

    Add API request with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+
+  onSearchSubmit(term) {
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization: 'Client-ID UmBUTENy_-knJN2pyTwE8PQF-zJNiK2oGFdz9rLfNPQ',
+        }
+      }).then((response) => {
+        console.log(response.data.results);
+      });
+  }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
-};
+}
 
 export default App;
