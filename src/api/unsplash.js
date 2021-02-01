4d2371105837b4990857bb4eae341400ca5d3a9f
Ugonz86 <ugonzalez86@gmail.com>
2021-02-01 15:43:56 -0800

    Add minor updates

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 
+require('dotenv').config()
+
+const API_KEY = `${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`;
+console.log(`${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`);
+console.log(API_KEY);
+console.log(process.env);
+
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization:
-            'Client-ID UmBUTENy_-knJN2pyTwE8PQF-zJNiK2oGFdz9rLfNPQ',
+        Authorization: API_KEY
     }
 });
